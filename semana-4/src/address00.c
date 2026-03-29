129fb7fbee241fca12675aef7b34758689664554
Alexandre Rademaker <arademaker@gmail.com>
2026-03-29 18:48:40 -0300

    reorganizacao arquivos

--- semana-4/src/address00.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-
-int main(void) {
-  
-    int n = 50;
-    printf("%d\n", n);
-}
---
+++ semana-4/src/scores5.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]) {
+
+  if (argc != 2) {
+    printf("usage: %s <n>\n", argv[0]);
+    return 1;
+  }
+
+  int n = atoi(argv[1]);
+  int scores[n]; 
+  int total = 0;
+  
+  for(int i = 0; i < n; i++) {
+    printf("value [%d]: ", i);
+    scanf("%d", &scores[i]);
+    total = total + scores[i];    
+  }
+    
+  printf("average: %f\n", total / (float) n);
+}
